fed02264f435f03261f46330b4efabeeac089db7
2627660832@qq.com <sez1993sez>
2019-07-29 08:47:54 +0800

    add files

---
+++ WebFormsPW/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace WebFormsPW.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ WebFormsPW/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebFormsPW.Global" Language="C#" %>
